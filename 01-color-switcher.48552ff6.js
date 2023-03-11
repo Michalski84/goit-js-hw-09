1eea3dbca466f45525ab4e3182ed48cdcd31d44b
Michalski84 <113372492+Michalski84@users.noreply.github.com>
2023-03-11 15:20:25 +0000

    Deploying to gh-pages from @ Michalski84/goit-js-hw-09@a37bec3f6b52724d12be6b1b60f051e42533c9c8 🚀

---
+++ 01-color-switcher.48552ff6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,bgColorID=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(bgColorID),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.48552ff6.js.map
