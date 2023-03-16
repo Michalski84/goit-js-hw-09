cbf89f80805792844d99e3f7a331a9703f8700f0
Michalski84 <113372492+Michalski84@users.noreply.github.com>
2023-03-16 20:36:06 +0000

    Deploying to gh-pages from @ Michalski84/goit-js-hw-09@908b120dfa970e01caf3b2504f01cdf341d6fc79 🚀

--- 01-color-switcher.715abf88.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,bgColorID=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(bgColorID),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.715abf88.js.map
