908b120dfa970e01caf3b2504f01cdf341d6fc79
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2023-03-16 21:35:33 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 const bgColor = document.querySelector('body');
+let bgColorID;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
